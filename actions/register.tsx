675aacc7b0e75b2606e76f4f48cc8ac04b3f7eb1
Grajaal <jorgegrajal@gmail.com>
2024-11-12 19:46:02 +0100

    register implemented

--- actions/register.tsx
+++ actions/register.tsx
@@ -1,7 +1,11 @@
 "use server";
 
+import bcryptjs from "bcryptjs";
+import { db } from "@/lib/db";
+
 import * as z from "zod";
 import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -9,6 +13,26 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   if (!validatedFields.success) {
     return { error: "Faltan campos por completar." };
   }
+
+  const { name, email, password, role } = validatedFields.data;
+  const hashedPassword = await bcryptjs.hash(password, 10);
+
+  const emailExists = await getUserByEmail(email);
+
+  if (emailExists) {
+    return { error: "Ese correo electrónico ya está en uso." };
+  }
+
+  console.log(role);
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+      role,
+    },
+  });
 
   return { success: "Registro completado." };
 };
