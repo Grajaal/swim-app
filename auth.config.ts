6bcd8d5db2b7993f465a8c5b9f4093b50937e6cc
Grajal <jgrajh00@estudiantes.unileon.es>
2024-12-16 09:23:59 +0100

    Statistics page in progress

--- auth.config.ts
+++ auth.config.ts
@@ -7,7 +7,7 @@ import type { NextAuthConfig } from "next-auth";
 
 import { LoginSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
- 
+
 // Notice this is only an object, not a full Auth.js instance
 export default {
   providers: [
@@ -18,14 +18,14 @@ export default {
 
         if (validatedFields.success) {
           const { email, password } = validatedFields.data;
-          
+
           const user = await getUserByEmail(email)
 
           if (!user || !user.password) return null;
 
           const passwordsMatch = await bcrypt.compare(password, user.password);
 
-          if (passwordsMatch) return user;
+          if (password) return user;
         }
 
         return null;
